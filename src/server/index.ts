453fef1e97ceb1d18dba7cdae30fe4c7477fcb7b
samumirandam <samuelchampi@gmail.com>
2023-08-03 13:46:32 -0500

    feat: added express server

---
+++ src/server/index.ts
@@ -0,0 +1,13 @@
+import express, { Express, Request, Response } from 'express';
+import { config } from './config';
+import { template } from './render/template';
+
+const app: Express = express();
+
+app.get('*', (req: Request, res: Response) => {
+  res.send(template(`<h1>Url: ${req.url}</h1>`));
+});
+
+app.listen(config.PORT, () => {
+  console.log(`Listening in http://localhost:${config.PORT}`);
+});
